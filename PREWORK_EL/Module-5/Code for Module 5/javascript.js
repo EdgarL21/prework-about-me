f653a8f6bfcaa58e267d7791cc89f42724c59dd7
EdgarL21 <80562742+EdgarL21@users.noreply.github.com>
2021-03-16 23:17:18 -0700

    Add files via upload

---
+++ PREWORK_EL/Module-5/Code for Module 5/javascript.js
@@ -0,0 +1,24 @@
+document.getElementById("button1").addEventListener("click", function(){
+    document.getElementById("box").style.height = "250px";
+});
+document.getElementById("button2").addEventListener("click", function(){
+    document.getElementById("box").style.backgroundColor = "blue";
+});
+document.getElementById("button3").addEventListener("click", function(){
+    document.getElementById("box").style.display = "none";
+});
+document.getElementById("button4").addEventListener("click", function(){
+    document.getElementById("box").style.height = "150px";
+});
+document.getElementById("button4").addEventListener("click", function(){
+    document.getElementById("box").style.backgroundColor = "orange";
+});
+document.getElementById("button4").addEventListener("click", function(){
+    document.getElementById("box").style.display = "block";
+});
+document.getElementById("button5").addEventListener("click", function(){
+    document.getElementById("box").style.border = "thick dotted #FF00FF";
+});
+document.getElementById("button4").addEventListener("click", function(){
+    document.getElementById("box").style.border = "none";
+});
